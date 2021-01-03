6364db0d17e95b22ac4a39ecb236fd47a4d964b0
JoshSevy <joshuasevy@outlook.com>
2021-01-03 01:16:20 -0700

    Add first set of problems

---
+++ problems.js
@@ -0,0 +1,123 @@
+//Setting and Sweeping
+// Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
+
+
+
+
+//Print -52 to 1066
+// Print integers from -52 to 1066 using a FOR loop.
+
+
+
+
+// Print and Count
+// Print all integer multiples of 5 from 512 to 4096. Afterward, also log how many there were.
+
+export function printAndCount() {
+
+}
+
+
+// Multiples of Six
+// Print multiples of 6 up to 60,000, using a while loop.
+
+export function multiSix() {
+
+}
+
+
+// Don't worry, Be happy
+// Create beCheerful(). Within it, console.log string "good morning!". Call it 98 times.
+
+export function beCheerful() {
+
+}
+
+
+//What do you know?
+// Your function will be given an input parameter incoming. Please console.log this value.
+
+export function doYouKnow(incoming) {
+
+}
+
+
+
+//Counting, the Dojo Way
+// Print integers 1 to 100. If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
+
+export function dojoWay() {
+
+}
+
+
+//Multiples of Three - but Not All
+// Using FOR, print multiples of 3 from -300 to 0.
+// Skip -3 and -6.
+
+export function someThrees() {
+
+}
+
+
+//Printing Integers with While
+// Print integers from 2000 to 5280 using a WHILE
+
+export function mileHigh() {
+
+}
+
+
+//Whoa, That Sucker's Huge...
+// Add odd integers from -300,000 to 300,000, and console.log the final sum.
+
+export function hugeSucker() {
+
+}
+
+
+//Countdown by Fours
+//Log positive numbers starting at 2016, counting down by fours (exclude 0), without a FOR loop.
+
+export function fourCountdown() {
+
+}
+
+
+
+//Leap Year
+/*Write a function that determines whether a given year is a leap year.
+If a year is divisible by four, it is a leap year, unless it is divisible by 100. However, if it is divisible by 400, then it is.
+*/
+
+export function leapYear() {
+
+}
+
+
+//Flexible Countdown
+/*
+Based on earlier "Countdown by Fours", given lowNum, highNum, mult,
+print multiples of mult from highNum down to lowNum, using a FOR loop.
+For (2,9,3), print 9 6 3 (on successive lines)
+*/
+
+export function flexCountdown() {
+
+}
+
+
+//The Final Countdown
+/*
+This is based on "Flexible Countdown". The parameter names are not as helpful,
+but the problem is essentially identical; don't be thrown off!
+Given 4 parameters (param1, param2, param3, param4) print the multiples of param1,
+starting at param2 and extending to param3. One exception: if a multiple is equal
+to param4, then skip (don't print) it. Do this using a WHILE loop.
+Given (3, 5, 17, 9), print 6, 12, 15
+(which are all multiples of 3 between 5 and 17, and excluding the value 9).
+*/
+
+export function finalCountdown() {
+
+}
